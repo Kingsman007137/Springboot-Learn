1f221eeae348d314867d2b87ed3d281b48596897
pan <3398530682@qq.com>
2024-01-23 10:45:39 +0800

    配置文件

--- Tlias/src/main/java/com/example/utils/AliOSSUtils.java
+++ Tlias/src/main/java/com/example/utils/AliOSSUtils.java
@@ -5,6 +5,8 @@ import com.aliyun.oss.OSSClientBuilder;
 import com.aliyun.oss.common.auth.CredentialsProviderFactory;
 import com.aliyun.oss.common.auth.EnvironmentVariableCredentialsProvider;
 import com.aliyuncs.exceptions.ClientException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.*;
@@ -16,12 +18,16 @@ import java.util.UUID;
 @Component
 public class AliOSSUtils {
 
-    private String endpoint = "https://oss-cn-guangzhou.aliyuncs.com";
 //    private String accessKeyId = "LTAI4GCH1vX6DKqJWxd6nEuW";
 //    private String accessKeySecret = "yBshYweHOpqDuhCArrVHwIiBKpyqSL";
     // 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
     EnvironmentVariableCredentialsProvider credentialsProvider = CredentialsProviderFactory.newEnvironmentVariableCredentialsProvider();
-    private String bucketName = "tlias-kingsman";
+//    @Value("${aliyun.oss.endpoint}")
+//    private String endpoint;
+//    @Value("${aliyun.oss.bucketName}")
+//    private String bucketName;
+    @Autowired
+    private AliOSSProperties aliOSSProperties;
 
     public AliOSSUtils() throws ClientException {
     }
@@ -30,6 +36,10 @@ public class AliOSSUtils {
      * 实现上传图片到OSS
      */
     public String upload(MultipartFile file) throws IOException {
+        //获取阿里云OSS参数
+        String endpoint = aliOSSProperties.getEndpoint();
+        String bucketName = aliOSSProperties.getBucketName();
+
         // 获取上传的文件的输入流
         InputStream inputStream = file.getInputStream();
 
